5952088a0c93546bd36857e380570f6494608a01
Ajit Chaudhary <chaudharyajit953@gmail.com>
2026-05-21 12:07:06 +0530

    More in Rest Api

---
+++ day-3/src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+
+const notes = []
+
+app.post('/notes', (req, res) => {
+
+    console.log(req.body)
+    notes.push(req.body)
+    res.send('note created')
+})
+
+
+app.get('/notes', (req, res) => {
+    res.send(notes)
+})
+
+
+app.delete('/notes/:index', (req, res) => {
+    delete notes[req.params.index]             // res.params = {index : indexof notes}
+    res.send('note deleted successfully')      // ex notes/2 --> {index : 2}
+                                               
+})
+
+
+app.patch('/notes/:index', (req, res) => {
+    notes[req.params.index].description = req.body.description
+    res.send('note updated successfully')
+})
+
+
+module.exports = app
