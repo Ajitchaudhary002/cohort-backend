64f4f8ef1226b3be428270544502b2b8c6b03924
Ajit Chaudhary <chaudharyajit953@gmail.com>
2026-06-28 19:18:37 +0530

    Implement user identification middleware and refactor post controllers to utilize it

--- Insta-clone/src/controllers/post.controllers.js
+++ Insta-clone/src/controllers/post.controllers.js
@@ -9,27 +9,7 @@ const imagekit = new ImageKit({
 
 
 async function createPostController(req, res) {
-    console.log(req.body, req.file)
-    const token = req.cookies.token
-
-    if (!token) {
-        return res.status(401).json({
-            message: "Token not provided, unauthorized access"
-        })
-    }
-
-    let decoded;
-
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (err) {
-        return res.status(401).json({
-            message: 'user not authorized'
-        })
-    }
-
-    console.log(decoded);
-
+   
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
         fileName: 'test',
@@ -39,7 +19,7 @@ async function createPostController(req, res) {
     const post = await postModel.create({
         caption: req.body.caption,
         imgUrl: file.url,
-        user: decoded.id
+        user: req.user.id
     })
 
     res.status(201).json({
@@ -49,25 +29,8 @@ async function createPostController(req, res) {
 }
 
 async function getPostController(req, res) {
-    const token = req.cookies.token;
 
-    if (!token) {
-        return res.status(401).json({
-            message: "Token not provided, unauthorized access"
-        })
-    }
-
-    let decoded;
-
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (err) {
-        return res.status(401).json({
-            message: 'User not authorized, Invalid token'
-        })
-    }
-
-    const userId = decoded.id;
+    const userId = req.user.id
 
     const posts = await postModel.find({
         user: userId
@@ -81,26 +44,8 @@ async function getPostController(req, res) {
 }
 
 async function getPostDetailsController(req, res) {
-    const token = req.cookies.token;
 
-    if (!token) {
-        return res.status(401).json({
-            message: "UnAuthorized Access"
-
-        })
-    }
-
-    let decoded;
-
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (err) {
-        return res.status(401).json({
-            message: "Invalid Token"
-        })
-    }
-
-    const userId = decoded.id;
+    const userId = req.user.id
     const postId = req.params.postId
 
     const post = await postModel.findById(postId)
--- Insta-clone/src/routes/post.routes.js
+++ Insta-clone/src/routes/post.routes.js
@@ -3,11 +3,13 @@ const postRouter = express.Router();
 const postControllers = require('../controllers/post.controllers')
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() })
+const identifyUser = require('../middlewares/post.middleware')
 
-postRouter.post('/', upload.single('image'), postControllers.createPostController)
 
-postRouter.get('/', postControllers.getPostController)
+postRouter.post('/', upload.single('image'), identifyUser, postControllers.createPostController)
 
-postRouter.get('/details/:postId', postControllers.getPostDetailsController)
+postRouter.get('/', identifyUser, postControllers.getPostController)
+
+postRouter.get('/details/:postId', identifyUser, postControllers.getPostDetailsController)
 
 module.exports = postRouter
