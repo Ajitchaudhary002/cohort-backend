df156dd520e509edd87513c53bc48a8e269358e3
Ajit Chaudhary <chaudharyajit953@gmail.com>
2026-07-14 16:34:41 +0530

    feat: implement logout functionality and blacklist token management

--- Moodify/Backend/src/controllers/auth.controller.js
+++ Moodify/Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model')
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const blacklistModel = require('../models/blacklist.model')
 
 
 async function registerUser(req, res) {
@@ -109,10 +110,25 @@ async function getMe(req, res) {
     })
 }
 
+async function logoutUser(req, res) {
+    const token = req.cookies.token;
+
+    res.clearCookie("token")
+
+    await blacklistModel.create({
+        token
+    })
+
+    res.status(200).json({
+        message: "Logout successfully"
+    })
+}
+
 
 module.exports = {
     registerUser,
     loginUser,
-    getMe
+    getMe,
+    logoutUser
 }
 
