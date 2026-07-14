da4951a3480ef2a28037e701a1e3af43ff56f6d8
Ajit Chaudhary <chaudharyajit953@gmail.com>
2026-07-14 15:44:06 +0530

    feat: implement user authentication with registration, login, and token verification

--- Moodify/moodify/Backend/src/app.js
+++ Moodify/moodify/Backend/src/app.js
@@ -5,5 +5,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 
+// TASK
+// userSchema.pre("save", function (next) { })
+// userSchema.post("save", function (next) { })
+
+const authRoutes = require('./routes/auth.routes')
+
+app.use('/api/auth', authRoutes)
 
 module.exports = app
