9a7bc9d38bc3a8000525e04164e022b0d995dd8c
Ajit Chaudhary <chaudharyajit953@gmail.com>
2026-07-27 20:50:19 +0530

    complete frontend auth with login, register, getMe, and Redux integration

---
+++ Perplexity/Frontend/src/app/App.jsx
@@ -0,0 +1,19 @@
+import { router } from "./app.routes"
+import { RouterProvider } from "react-router"
+import { useEffect } from "react"
+import { useAuth } from "../features/auth/hook/useAuth"
+
+const App = () => {
+
+  const auth = useAuth()
+
+  useEffect(() => {
+    auth.handleGetMe()
+  }, [])
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
