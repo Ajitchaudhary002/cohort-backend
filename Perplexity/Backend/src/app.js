6bd1e94cd6a8a888dd87b80e0aadde2be16b2374
Ajit Chaudhary <chaudharyajit953@gmail.com>
2026-07-29 18:55:13 +0530

    implement get chats, get messages, and delete chat APIs

--- Perplexity/Backend/src/app.js
+++ Perplexity/Backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import authRouter from './routes/auth.routes.js'
+import chatRouter from './routes/chat.routes.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import morgan from 'morgan'
@@ -17,6 +18,7 @@ app.use(cors({
 
 
 app.use('/api/auth', authRouter)
+app.use('/api/chats', chatRouter)
 
 
 export default app
