e83d3f13f9e14fa7404096c3f37f6f8f683e6c15
Ajit Chaudhary <chaudharyajit953@gmail.com>
2026-08-12 12:09:12 +0530

    integrate Tavily API for internet search functionality
    - Added @tavily/core dependency to the backend.
    - Implemented searchInternet function in internet.service.js to utilize Tavily's search capabilities.
    - Updated ai.service.js to include a new searchInternet tool for the agent.
    - Enhanced message handling in useChat.js to support chat creation and message retrieval.
    - Updated Dashboard.jsx to handle chat opening with message retrieval.
    - Added remark-gfm dependency to the frontend for enhanced markdown support.
    - Updated index.css to hide scrollbar for messages.

--- Perplexity/Backend/src/services/ai.service.js
+++ Perplexity/Backend/src/services/ai.service.js
@@ -1,33 +1,60 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { HumanMessage, SystemMessage, AIMessage } from 'langchain'
+import { HumanMessage, SystemMessage, AIMessage, tool, createAgent } from 'langchain'
 import { ChatMistralAI } from '@langchain/mistralai'
 import userModel from "../models/user.model.js";
-
+import * as z from 'zod';
+import { searchInternet } from "../services/internet.service.js";
 
 const mistralModel = new ChatMistralAI({
     model: "mistral-small-latest",
     apiKey: process.env.MISTRAL_API_KEY
 })
 
-
 const geminiModel = new ChatGoogleGenerativeAI({
     model: "gemini-3-flash-preview",
     apiKey: process.env.GEMINI_API_KEY
 });
 
+const searchInternetTool = tool(
+    searchInternet,
+    {
+        name: "searchInternet",
+        description: "A tool for searching the internet for information.",
+        schema: z.object({
+            query: z.string().describe("The search query to look up on the internet.")
+        })
+    })
+
+const agent = createAgent({
+    tools: [searchInternetTool],
+    model: geminiModel,
+    agentType: "chat-conversational-react-description",
+    verbose: true
+})
+
+
 
 export async function generateResponse(messages) {
-    const response = await geminiModel.invoke(messages.map(msg => {
-        if (msg.role = 'user') {
-            return new HumanMessage(msg.content)
-        } else if (msg.role = 'ai') {
-            return new AIMessage(msg.content)
-        }
-    }))
+    console.log(messages)
 
-    return response.text
+    const response = await agent.invoke({
+        messages: [
+            new SystemMessage(`
+                You are a helpful and precise assistant for answering questions.
+                If you don't know the answer, say you don't know. 
+                If the question requires up-to-date information, use the "searchInternet" tool to get the latest information from the internet and then answer based on the search results.
+            `),
+            ...(messages.map(msg => {
+                if (msg.role == "user") {
+                    return new HumanMessage(msg.content)
+                } else if (msg.role == "ai") {
+                    return new AIMessage(msg.content)
+                }
+            }))]
+    });
+
+    return response.messages[response.messages.length - 1].text;
 }
-
 
 export async function generateChatTitle(message) {
     const response = await mistralModel.invoke([
